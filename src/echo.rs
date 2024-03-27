af725af95045348b435cf8b7e98f49cb6eb39676
askepen <aske@vhg26.dk>
2024-03-27 10:49:41 +0100

    random broadcast messages

--- src/echo.rs
+++ src/echo.rs
@@ -1,3 +1,4 @@
+use rand::seq::IteratorRandom;
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use std::io::Write;
@@ -98,6 +99,7 @@ enum Response {
     BroadcastOk,
 }
 
+#[derive(Debug)]
 struct Node {
     /// The id of this node
     id: NodeId,
@@ -108,7 +110,14 @@ struct Node {
     /// Neighboring nodes in the cluster (including this node) and which values we know that they have seen
     cluster: HashMap<NodeId, HashSet<MsgValue>>,
 
-    outbound_requests: HashMap<MsgId, Request>,
+    /// We only have one outstanding request to each node at a time
+    outbound_requests: HashMap<MsgId, OutboundRequest>,
+}
+
+#[derive(Debug)]
+struct OutboundRequest {
+    request: Request,
+    dest: NodeId,
 }
 
 impl Node {
@@ -134,89 +143,131 @@ impl Node {
         }
     }
 
+    fn missing_values(&self) -> HashMap<NodeId, HashSet<MsgValue>> {
+        let all_values = self.cluster[&self.id].iter().cloned().collect::<HashSet<_>>();
+        self.cluster
+            .iter()
+            .map(|(id, seen_values)| {
+                let unseen_values = all_values.difference(seen_values);
+                (id.clone(), unseen_values.cloned().collect())
+            })
+            .collect()
+    }
+
     fn broadcast(&mut self) {
         let mut last_msg_id = self.last_msg_id;
         let all_values = self.cluster[&self.id].iter().cloned().collect::<HashSet<_>>();
+        let mut rng = rand::thread_rng();
         for (dest, seen_values) in self.cluster.iter_mut() {
             let unseen_values = all_values.difference(seen_values);
 
-            for value in unseen_values {
-                let msg_id = last_msg_id + 1;
-                last_msg_id += 1;
+            // Pick a random unseen value to broadcast
+            let Some(value) = unseen_values.into_iter().choose(&mut rng) else {
+                continue;
+            };
 
-                let request = Request::Broadcast { value: *value };
-                let msg = Message {
-                    src: self.id.clone(),
+            let msg_id = last_msg_id + 1;
+            last_msg_id += 1;
+
+            let request = Request::Broadcast { value: *value };
+            let msg = Message {
+                src: self.id.clone(),
+                dest: dest.clone(),
+                body: MessageBody::Request {
+                    request: request.clone(),
+                    msg_id,
+                },
+            };
+            msg.send();
+            self.outbound_requests.insert(
+                msg_id,
+                OutboundRequest {
+                    request,
                     dest: dest.clone(),
-                    body: MessageBody::Request {
-                        request: request.clone(),
-                        msg_id: last_msg_id + 1,
-                    },
-                };
-                self.outbound_requests.insert(msg_id, request);
-                msg.send();
-            }
+                },
+            );
         }
         self.last_msg_id = last_msg_id;
     }
 
+    fn handle_request(&mut self, request: &Request, _msg_id: MsgId, src: &NodeId) -> Response {
+        match request {
+            Request::Init { .. } => panic!("unexpected Init request"),
+            Request::Echo { echo } => Response::EchoOk { echo: echo.clone() },
+            Request::Generate => Response::GenerateOk { id: Uuid::new_v4() },
+            Request::Broadcast { value } => {
+                // We know that we have seen this value
+                let our_seen_values = self.cluster.get_mut(&self.id).expect("own id is in cluster");
+                our_seen_values.insert(*value);
+                let our_seen_values_len = our_seen_values.len();
+
+                // We know that the sender has seen this value
+                if let Some(seen_values) = self.cluster.get_mut(src) {
+                    seen_values.insert(*value);
+                }
+
+                // if self.outbound_requests.len() < self.cluster.len() * our_seen_values_len {
+                //     self.broadcast();
+                // }
+                Response::BroadcastOk
+            }
+            Request::Read => Response::ReadOk {
+                values: self.cluster[&self.id].clone(),
+            },
+            Request::Topology { topology } => {
+                for id in topology.get(&self.id).expect("our own id is in topology") {
+                    if !self.cluster.contains_key(id) {
+                        self.cluster.insert(id.clone(), HashSet::new());
+                    }
+                }
+                Response::TopologyOk
+            }
+        }
+    }
+
+    fn handle_response(&mut self, response: Response, src: NodeId, in_reply_to: MsgId) {
+        if let Some(OutboundRequest {
+            request: outbound_req, ..
+        }) = self.outbound_requests.remove(&in_reply_to)
+        {
+            match (&outbound_req, &response) {
+                (Request::Broadcast { value }, Response::BroadcastOk) => {
+                    // We know that the destination has seen this value if they replied with BroadcastOk
+                    self.cluster
+                        .get_mut(&src)
+                        .expect("destination id in cluster")
+                        .insert(*value);
+                }
+                _ => panic!("Request and Response do not match: {:?} {:?}", outbound_req, response),
+            }
+        }
+    }
+
     fn run(mut self) {
-        let stdin = std::io::stdin();
-        let deserializer = serde_json::Deserializer::from_reader(stdin);
+        let deserializer = serde_json::Deserializer::from_reader(std::io::stdin().lock());
         for msg in deserializer.into_iter::<Message>() {
-            let msg = msg.expect("failed to parse message");
+            let msg = msg.expect("a valid message");
             if msg.dest != self.id {
                 continue;
             }
+            let src = msg.src.clone();
             match msg.body {
-                MessageBody::Request { ref request, .. } => match request {
-                    Request::Init { .. } => panic!("unexpected Init request"),
-                    Request::Echo { echo } => msg.respond_with(Response::EchoOk { echo: echo.to_string() }),
-                    Request::Generate => msg.respond_with(Response::GenerateOk { id: Uuid::new_v4() }),
-                    Request::Broadcast { value } => {
-                        // We know that we have seen this value
-                        self.cluster
-                            .get_mut(&self.id)
-                            .expect("own id is in cluster")
-                            .insert(*value);
+                MessageBody::Request { ref request, msg_id } => {
+                    let response = self.handle_request(request, msg_id, &src);
 
-                        // We know that the sender has seen this value
-                        self.cluster
-                            .get_mut(&msg.src)
-                            .map(|seen_values| seen_values.insert(*value));
+                    msg.respond_with(response);
 
-                        msg.respond_with(Response::BroadcastOk);
-                        self.broadcast();
-                    }
-                    Request::Read => msg.respond_with(Response::ReadOk {
-                        values: self.cluster[&self.id].clone(),
-                    }),
-                    Request::Topology { topology } => {
-                        for id in topology.get(&self.id).expect("our own id is in topology") {
-                            if !self.cluster.contains_key(id) {
-                                self.cluster.insert(id.clone(), HashSet::new());
-                            }
-                        }
-                        msg.respond_with(Response::TopologyOk);
-                    }
-                },
-                MessageBody::Response { response, in_reply_to } => match response {
-                    Response::BroadcastOk => {
-                        match self.outbound_requests.remove(&in_reply_to) {
-                            Some(Request::Broadcast { value }) => {
-                                // We know that the destination has seen this value if they replied with BroadcastOk
-                                self.cluster
-                                    .get_mut(&msg.src)
-                                    .expect("destination id in cluster")
-                                    .insert(value);
-                            }
-                            Some(req) => panic!("unexpected request {:?}", req),
-                            None => {}
-                        }
-                    }
-                    _ => panic!("unexpected response {:?}", response),
-                },
+                    // We assume that the sender has not seen our outbound requests,
+                    // so we can give up on them when we receive a response
+                    self.outbound_requests
+                        .retain(|_, OutboundRequest { dest, .. }| dest.clone() != src);
+                }
+                MessageBody::Response { response, in_reply_to } => self.handle_response(response, msg.src, in_reply_to),
             }
+        }
+        let num_seen_values = self.cluster.get(&self.id).expect("own id is in cluster").len();
+        if (self.outbound_requests.len() as f64) < (self.cluster.len() as f64) * (num_seen_values as f64).sqrt() {
+            self.broadcast();
         }
     }
 }
