6d3adc468518882e030ab33cade9b238ec4357cd
askepen <aske@vhg26.dk>
2024-03-31 14:30:58 +0200

    3d

--- src/workloads/broadcast.rs
+++ src/workloads/broadcast.rs
@@ -1,9 +1,13 @@
+use rand::seq::IteratorRandom;
 use serde::{Deserialize, Serialize};
 
 use crate::workloads::workload::Workload;
 use crate::{message, node::NodeId};
+use rand::{self, thread_rng};
 use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::Sender;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use super::workload::Body;
 
@@ -35,20 +39,36 @@ pub enum Response {
         #[serde(rename = "messages")]
         values: HashSet<MsgValue>,
     },
-    GossipOk,
+}
+
+struct BroadcastState {
+    tx: Sender<Body<BroadcastWorkload>>,
+    seen_values: HashSet<MsgValue>,
+    to_broadcast: HashSet<MsgValue>,
+    neighbors: HashSet<NodeId>,
+    all_nodes: HashSet<NodeId>,
 }
 
 pub struct BroadcastWorkload {
     id: NodeId,
-    tx: Sender<Body<Self>>,
-    outbound_requests: HashMap<message::MsgId, OutboundRequest>,
-    seen_values: HashMap<NodeId, HashSet<MsgValue>>,
+    state: Arc<Mutex<BroadcastState>>,
 }
 
-#[derive(Debug)]
-struct OutboundRequest {
-    value: HashSet<MsgValue>,
-    dest: NodeId,
+impl BroadcastState {
+    fn gossip(&mut self, rng: &mut rand::rngs::ThreadRng) {
+        let values: HashSet<_> = self.to_broadcast.union(&self.seen_values).cloned().collect();
+
+        for dest in self.all_nodes.iter().choose_multiple(rng, 4) {
+            let request = Body::Request {
+                dest: dest.clone(),
+                request: Request::Gossip { values: values.clone() },
+            };
+            self.tx.send(request).expect("send failed");
+        }
+
+        self.seen_values.extend(self.to_broadcast.clone().into_iter());
+        self.to_broadcast.clear();
+    }
 }
 
 impl Workload for BroadcastWorkload {
@@ -56,73 +76,70 @@ impl Workload for BroadcastWorkload {
     type Response = Response;
 
     fn new(id: &NodeId, tx: Sender<Body<Self>>) -> Self {
-        BroadcastWorkload {
-            id: id.clone(),
+        let state = Arc::new(Mutex::new(BroadcastState {
             tx,
-            outbound_requests: HashMap::new(),
-            seen_values: HashMap::from([(id.clone(), HashSet::new())]),
-        }
+            seen_values: Default::default(),
+            to_broadcast: Default::default(),
+            neighbors: Default::default(),
+            all_nodes: Default::default(),
+        }));
+
+        let state_gossip = state.clone();
+        thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+            loop {
+                thread::sleep(std::time::Duration::from_millis(100));
+                state_gossip.lock().unwrap().gossip(&mut rng);
+            }
+        });
+
+        BroadcastWorkload { id: id.clone(), state }
     }
 
     fn handle_request(
         &mut self,
         request: &Self::Request,
-        src: &NodeId,
+        _src: &NodeId,
         reponse_factory: impl FnOnce(Self::Response) -> Body<Self>,
     ) {
+        let mut state = self.state.lock().unwrap();
         match request {
             Request::Topology { topology } => {
-                self.seen_values
-                    .extend(topology[&self.id].iter().cloned().map(|id| (id, HashSet::new())));
-                self.tx
+                state.neighbors.extend(topology[&self.id].clone().into_iter());
+                state.all_nodes.extend(topology.keys().cloned());
+                state
+                    .tx
                     .send(reponse_factory(Response::TopologyOk))
                     .expect("send failed");
             }
             Request::Broadcast { value } => {
-                // We have now seen this value
-                self.seen_values
-                    .get_mut(&self.id)
-                    .expect("own id should be in seen_values")
-                    .insert(*value);
+                // Only broadcast if we haven't seen this value before
+                if !state.seen_values.contains(value) {
+                    state.seen_values.insert(*value);
+                    state.gossip(&mut thread_rng());
+                }
 
-                // and we know that the sender has also seen this value
-                self.seen_values
-                    .get_mut(src)
-                    .map(|seen_values| seen_values.insert(*value));
-
-                self.tx
+                state
+                    .tx
                     .send(reponse_factory(Response::BroadcastOk))
                     .expect("send failed");
             }
             Request::Read => {
-                self.tx
+                state
+                    .tx
                     .send(reponse_factory(Response::ReadOk {
-                        values: self.seen_values[&self.id].clone(),
+                        values: state.seen_values.clone(),
                     }))
                     .expect("send failed");
             }
             Request::Gossip { values } => {
-                self.seen_values
-                    .get_mut(&self.id)
-                    .expect("own id should be in seen_values")
-                    .extend(values.into_iter());
-
-                self.tx.send(reponse_factory(Response::GossipOk)).expect("send failed");
+                let unseen_values = values - &state.seen_values;
+                state.to_broadcast.extend(unseen_values.into_iter());
             }
         }
     }
 
-    fn handle_response(&mut self, response: &Response, in_reply_to: message::MsgId, _src: &NodeId) {
-        match response {
-            Response::BroadcastOk | Response::GossipOk => {
-                if let Some(outbound_req) = self.outbound_requests.remove(&in_reply_to) {
-                    self.seen_values
-                        .get_mut(&outbound_req.dest)
-                        .expect("src should be in cluster")
-                        .extend(outbound_req.value);
-                }
-            }
-            _ => panic!("Did not expect response of type {:?}", response),
-        }
+    fn handle_response(&mut self, response: &Response, _in_reply_to: message::MsgId, _src: &NodeId) {
+        panic!("Did not expect response of type {:?}", response);
     }
 }
